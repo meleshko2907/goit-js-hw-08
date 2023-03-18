e0faf43c835745dc1ec611fd34bd0bb0cbad88b2
Meleshko Aleksey <meleshko.aleksey1996@gmail.com>
2023-03-18 22:28:29 +0200

    add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Vimeo(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function onTimeUpdate(event) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(event.seconds));
+  console.log(event.seconds);
+}
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+if (currentTime) {
+  player
+    .setCurrentTime(currentTime)
+    .then(function (seconds) {
+      console.log(seconds);
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    }),
+    console.log(STORAGE_KEY);
+}
