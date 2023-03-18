993575240c3637bcd80e08ee64535784e78c1cd4
Meleshko Aleksey <meleshko.aleksey1996@gmail.com>
2023-03-18 22:32:40 +0200

    add changing

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+const MY_KEY = 'feedback-form-state';
+
+updateOutput();
+
+form.addEventListener(
+  'input',
+  throttle(function saveData() {
+    const data = {
+      email: this.email.value,
+      message: this.message.value,
+    };
+    const data1 = JSON.stringify(data);
+
+    localStorage.setItem(MY_KEY, data1);
+  }, 500)
+);
+
+function updateOutput() {
+  try {
+    const saveData = localStorage.getItem(MY_KEY);
+
+    if (saveData === null) {
+      return undefined;
+    } else {
+      const parseData = JSON.parse(saveData);
+      email.value = parseData.email;
+      message.value = parseData.message;
+    }
+  } catch (error) {
+    console.error('Get state error', error.message);
+  }
+}
+
+form.addEventListener('submit', fillingField);
+
+function fillingField(event) {
+  event.preventDefault();
+  console.dir({ email: email.value, message: message.value });
+
+  localStorage.removeItem(MY_KEY);
+  form.reset();
+}
